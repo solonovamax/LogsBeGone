6b13fd54081947ef68e99b7b386a5c9f88970f04
solonovamax <solonovamax@12oclockpoint.com>
2022-01-11 00:00:00 -0500

    ignoring chunk is gone 🦀

---
+++ src/main/java/ca/solostudios/antiignoringchunk/AntiIgnoringChunk.java
@@ -0,0 +1,12 @@
+package ca.solostudios.antiignoringchunk;
+
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class AntiIgnoringChunk implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // TODO: 2022-01-10 Initialization
+    }
+}
---
+++ src/main/java/ca/solostudios/antiignoringchunk/mixin/ClientChunkManagerMixin.java
@@ -0,0 +1,19 @@
+package ca.solostudios.antiignoringchunk.mixin;
+
+import net.minecraft.client.world.ClientChunkManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+
+@Mixin(ClientChunkManager.class)
+public class ClientChunkManagerMixin {
+    
+    @Redirect(method = "loadChunkFromPacket",
+              at = @At(value = "INVOKE",
+                       target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
+    public void ignoringChunkWarning(Logger logger, String message, Object p1, Object p2) {
+        logger.debug(message, p1, p2); // fuck you mojang
+    }
+}
