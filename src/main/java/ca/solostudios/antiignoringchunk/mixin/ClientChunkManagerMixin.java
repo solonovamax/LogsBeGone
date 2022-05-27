7303715c898f638735ba39e1700f1cafb4776f3d
solonovamax <solonovamax@12oclockpoint.com>
2022-05-26 22:36:31 -0400

    Update to 1.18.2

--- src/main/java/ca/solostudios/antiignoringchunk/mixin/ClientChunkManagerMixin.java
+++ src/main/java/ca/solostudios/antiignoringchunk/mixin/ClientChunkManagerMixin.java
@@ -1,7 +1,7 @@
 package ca.solostudios.antiignoringchunk.mixin;
 
 import net.minecraft.client.world.ClientChunkManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -12,7 +12,7 @@ public class ClientChunkManagerMixin {
     
     @Redirect(method = "loadChunkFromPacket",
               at = @At(value = "INVOKE",
-                       target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
+                       target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
     public void ignoringChunkWarning(Logger logger, String message, Object p1, Object p2) {
         logger.debug(message, p1, p2); // fuck you mojang
     }
