bc77d6e93be06235a447c24f4b542047d2113f06
solonovamax <solonovamax@12oclockpoint.com>
2024-12-16 01:58:52 -0500

    Code reformat, fix mc version
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- src/main/java/ca/solostudios/logsbegone/mixin/ClientChunkManagerMixin.java
+++ src/main/java/ca/solostudios/logsbegone/mixin/ClientChunkManagerMixin.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2024 solonovamax <solonovamax@12oclockpoint.com>
+ *
+ * The file ClientChunkManagerMixin.java is part of LogsBeGone
+ * Last modified on 16-12-2024 01:41 a.m.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * GRADLE-CONVENTIONS-PLUGIN IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package ca.solostudios.logsbegone.mixin;
 
 import net.minecraft.client.world.ClientChunkManager;
@@ -6,14 +33,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-
 @Mixin(ClientChunkManager.class)
 public class ClientChunkManagerMixin {
-    
-    @Redirect(method = "loadChunkFromPacket",
-              at = @At(value = "INVOKE",
-                       target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
-    public void ignoringChunkWarning(Logger logger, String message, Object p1, Object p2) {
-//        logger.debug(message, p1, p2); // fuck you mojang
+
+    @Redirect(
+            method = "loadChunkFromPacket",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"
+            )
+    )
+    public void ignoringChunkWarning(Logger logger, String message, Object arg1, Object arg2) {
+        // logger.debug(message, arg1, arg2); // fuck you mojang
     }
 }
